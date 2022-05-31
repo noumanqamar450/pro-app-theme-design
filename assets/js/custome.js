c9f3ceaa67397e23c1828c2f2268bfcc91f460e1
Nouman Qamar <mnouman450@gmail.com>
2022-05-31 23:07:11 +0500

    initial commit

---
+++ assets/js/custome.js
@@ -0,0 +1,14 @@
+function navCollapse() {
+    var navCol = document.getElementById("collapse");
+    navCol.classList.toggle("active");
+
+}
+
+
+
+
+function faqsCollapse(a) {
+    var faqCol = document.getElementsByClassName("block");
+    faqCol[a].classList.toggle("active");
+
+}
